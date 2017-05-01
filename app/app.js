1c54afeab8f7210a01e8512190c55fdcf40b1c6a
Patrick San Juan <psj.webdev@gmail.com>
2017-05-01 10:38:56 -0700

    initial commit

---
+++ app/app.js
@@ -0,0 +1,84 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const port = process.env.PORT || 3000;
+const mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/shortUrl';
+const app = express();
+
+/*
+ * MODELS
+ */
+var UrlSchema = new Schema({
+	uid: { type: Number, required: true },
+	url: { type: String, required: true }
+});
+
+var URL = mongoose.model('URLSchema', UrlSchema);
+
+/*
+ * FEATURES
+ */
+function createID() {
+	return Math.floor((Math.random() * (8999)) + 1000);
+}
+
+/*
+ * ROUTES 
+ */
+app.get('/', function(req, res) {
+	res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.get('/favicon.ico', function(req, res) {
+   res.sendStatus(204);
+});
+
+
+app.get('/:id', function(req, res) {
+	var id = req.params.id;
+	URL.findOne({ uid: id })
+		.exec(function(err, urlDoc) {
+			if(err) throw err;
+			return res.redirect(urlDoc.url);
+		});
+});
+
+app.get('/new/*', function(req, res) {
+	var url = req.params[0];
+	URL.findOne({ url: url })
+		.exec(function(err, result) {
+			if(err) throw err;
+			if(!result) {
+				var doc = new URL({
+					uid: createID(),
+					url: url
+				});
+				doc.save(function(err) {
+					if(err) throw err;
+					return res.json(doc);
+				});
+			} else {
+				return res.status(302).json(result);
+			}
+		})
+});
+
+// Connect to database
+mongoose.connect(mongoUri);
+mongoose.connection.on('error', function(err) {
+	console.error('MongoDB connection error: ' + err);
+	process.exit(-1);
+	}
+);
+
+// Use native promises
+mongoose.Promise = global.Promise;
+
+app.listen(port, function() {
+	console.log('short-url-ms listening on port ' + port);
+});
